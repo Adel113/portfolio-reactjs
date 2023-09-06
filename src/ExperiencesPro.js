8f1dbea0eb74d79cfed8f19b7928623a2ad8ac6c
Adel SIDI AHMED <adelsidiahmed2020@gmail.com>
2023-09-07 00:02:24 +0200

    style pour les pages à ajouter !!

--- src/ExperiencesPro.js
+++ src/ExperiencesPro.js
@@ -10,6 +10,8 @@ function ExperiencesPro (){
               <h2>
                EXPÉRIENCES PROFISSIONNELLES
               </h2>
+              <h3>Graphiste designer<span> (2018-2022)</span></h3>
+              
               <p style={Pstyle}>
                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
               </p>
@@ -17,57 +19,62 @@ function ExperiencesPro (){
               <div className="row">
                 <div className="col-md-4">
                   <h3 >
-                   Heading
+                  Employé Polyvalent en Restaurant Pizzeria<span> (08/2023-à ce jour)</span>
                   </h3>
-                  <p>
-                   Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                  </p> 
+                  <ul>
+                    <li>Prise de commandes des clients, traitement des réservations et fourniture de recommandations de menu</li>
+                    <li>Préparation et service des plats, y compris la préparation de pizzas, de pâtes et d'autres spécialités.</li>
+                    <li>Gestion des transactions financières, y compris l'encaissement et la manipulation des paiements.</li>
+                    <li>Entretien des normes d'hygiène alimentaire, de propreté et de sécurité dans la salle à manger et la cuisine.</li>
+                  </ul>
+                </div>
+                <div className="col-md-4">
+                  <h3 >
+                  Graphiste designer<span> (2018-2022)</span>
+                  </h3>
+                  <ul>
+                    <li>Création de maquettes attrayantes pour des panneaux publicitaires imprimés et numériques, en collaboration avec des clients pour répondre à leurs besoins de communication.</li>
+                    <li>Conception de cartes de visite professionnelles et mémorables pour les clients du magasin et d'autres entreprises locales.</li>
+                    <li>Maîtrise de logiciels de retouche d'image, tels que Adobe Photoshop, pour améliorer la qualité des photos utilisées dans les campagnes marketing et publicitaires.</li>
+                    <li>Collaboration avec des agences de publicité ou des clients directs pour fournir des services de conception graphique de haute qualité.</li>
+                  </ul>
+                </div>
+                <div className="col-md-4">
+                  <h3 >
+                    spécialiste du flockage<span> (2018-2022)</span>
+                  </h3>
+                  <ul>
+                    <li>Réalisation de travaux de flockage sur une variété de supports, y compris t-shirts, casquettes, sacs, etc.</li>
+                    <li>Utilisation de techniques avancées de flockage pour créer des designs personnalisés et de haute qualité.</li>
+                    <li>Gestion des commandes de flockage, y compris la coordination de la conception avec les clients, le choix des matériaux et l'exécution de la production.</li>
+                    <li>Connaissance approfondie des techniques de pressage à chaud, des machines de flockage et des matériaux.</li>
+                  </ul>
                 </div>
                 <div className="col-md-4">
                   <h3>
-                   Heading
+                  Propriétaire d'un Magasin de Crêpes et Glaces <span> 2019-2022</span>
                   </h3>
-                  <p>
-                   Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                  </p>
+                  <ul>
+                    <li>Gestion complète du magasin, y compris la supervision du personnel, la gestion des opérations quotidiennes, la planification des horaires et la gestion des stocks.</li>
+                    <li>Création et mise en œuvre de stratégies de marketing pour attirer de nouveaux clients et fidéliser la clientèle existante.</li>
+                    <li>Gestion des finances, y compris la budgétisation, la comptabilité et la gestion des coûts.</li>
+                    <li>Maintien des normes d'hygiène et de sécurité alimentaire pour garantir la satisfaction des clients.</li>
+                  </ul>
                  
                 </div>
                 <div className="col-md-4">
                   <h3>
-                   Heading
+                    Entraîneur de Vo-Vietnam<span> 2018-2022</span>
                   </h3>
-                  <p>
-                   Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                  </p>
+                  <ul>
+                    <li>Enseignement des techniques de Vo-Vietnam à des élèves de tous âges et niveaux de compétence.</li>
+                    <li>Organisation et supervision d'entraînements réguliers pour améliorer la force, la souplesse, la coordination et les compétences en autodéfense des élèves.</li>
+                    <li>Préparation des élèves pour des compétitions locales, nationales ou internationales, le cas échéant.</li>
+                    <li>Mise en place de programmes de formation personnalisés pour les élèves et suivi de leur progression.</li>
+                  </ul>
                 </div>
               </div>
-              <div className="row">
-               <div className="col-md-4">
-                  <h3>
-                   Heading
-                  </h3>
-                  <p>
-                   Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                  </p> 
-                </div>
-                <div className="col-md-4">
-                  <h3>
-                   Heading
-                  </h3>
-                  <p>
-                   Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                  </p>
-                </div>
-                <div className="col-md-4">
-                  <h3>
-                   Heading
-                  </h3>
-                  <p>
-                   Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                  </p>
-                 
-                </div>
-              </div>
+              
             </div>
           </div>  
          
--- src/Formations.js
+++ src/Formations.js
@@ -2,7 +2,7 @@
 
 const pStyle = {marginBottom: "60px"}
 const p1Style = {marginTop: "200px"}
-
+const divStyle = {border: "5px solid #000" , padding: "20px" , borderRadius: "50px" , listStyle: "none"}
 
 function Formations(){
     return (
@@ -16,37 +16,80 @@ function Formations(){
                        FORMATIONS
                     </h2>
                     <p style={pStyle}>
-                      Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                    </p>
+                    J'ai obtenu un baccalauréat en mathématiques avec mention bien, mais ai rapidement réalisé que ma véritable passion était l'informatique. J'ai donc décidé de me réorienter, validant ma première année en informatique et effectuant un stage de deux mois en tant que développeur web. Mon parcours académique et mon expérience en stage ont renforcé ma passion pour l'informatique et m'ont permis d'acquérir des compétences essentielles dans ce domaine. Je suis maintenant enthousiaste à l'idée de continuer à développer ma carrière dans le monde de la technologie.</p>
                 
                     <div className="row">
-                       <div className="col-md-4">
-                        <h3 >
-                         Licence 1 - university Mouloud maameri
-                        </h3>
-                        <p>
-                         Science Technologie : Math, Physics, chimie 
-                        </p>
-                       
-                       </div>
-                        <div className="col-md-4">
-                          <h3>
-                           Bac +1 ENIGMA Lille  
-                          </h3>
-                          <p>
-                           
-                           Au cours de ma formation à l'école d'informatique, j'ai suivi un programme complet et diversifié qui m'a permis d'acquérir une solide base de connaissances dans différents domaines de l'informatique et des technologies numériques. 
-                           Cette formation m'a préparé à relever les défis passionnants du monde de l'informatique et à explorer de nouvelles opportunités dans le domaine de la technologie.
-                           </p>
+                        <div className="col-md-4 " >
+                          <ul className="ligne" style={divStyle}>
+                            <li>
+                              <h3 >
+                                  Baccalaureat Mathématiques <span> (2020-2021)</span> 
+                                </h3>
+                            </li>
+                            <li>Cours de Mathématiques et calcul function et intégral...</li>
+                            <li>Travaux pratiques en Physique , incluant des expériences en laboratoire....</li>
+                            <li>Cours théoriques et travaux dirigés (TD) en génie civil...</li>
+                            <li>Travaux dirigés (TD) en Chimie...</li>
+                            <li>Cours de langues Français - Anglais...</li>
+                          </ul>
+                        </div>
+                        <div className="col-md-4 " >
+                          <ul className="ligne" style={divStyle}>
+                            <li>
+                              <h3 >
+                                  Licence 1 Sciences et technologie <span> (2021-2022)</span> 
+                                </h3>
+                            </li>
+                            <li>Cours de Mathématiques avancées en calcul différentiel et intégral.</li>
+                            <li>Travaux pratiques en Physique, incluant des expériences en laboratoire</li>
+                            <li>Cours théoriques et travaux dirigés (TD) en Chimie</li>
+                            <li>TD de Mathématiques pour renforcer la compréhension des concepts fondamentaux</li>
+                          </ul>
+                        </div>
+                        <div className="col-md-4 " >
+                          
+                          <ul className="ligne" style={divStyle}>
+                            <li>
+                              <h3>
+                                Bac +1 Enigma LILLE <span> (2022-2023)</span>  
+                               </h3>
+                            </li>
+                            <li>Programme complet et diversifié en informatique et technologies numériques.</li>
+                            <li>Acquisition d'une solide base de connaissances dans divers domaines informatiques.</li>
+                            <li>Préparation aux défis du monde de l'informatique.</li>
+                            <li>Exploration de nouvelles opportunités dans le domaine de la technologie.</li>
+                          </ul>
                        
                         </div>
+                        <div className="col-md-4 " >
+                          
+                          <ul className="ligne" style={divStyle}>
+                            <li>
+                                <h3>
+                                   Stage en Dev Web <span> (06/2023-08/2023)</span>
+                                </h3>
+                            </li>
+                            <li>Participation à la conception et au développement de sites web et d'applications web</li>
+                            <li>Programmation en utilisant des langages de développement web tels que HTML, CSS, JavaScript, et d'autres langages pertinents.</li>
+                            <li>Création et refonte de sites internet pour une expérience utilisateur optimale.</li>
+                            <li> Utilisation de Photoshop et Canva pour concevoir des visuels percutants et attrayants.</li>
+                            <li>Gestion des Réseaux Sociaux pour promouvoir les projets et établir des liens durables avec les utilisateurs et clients.</li>
+                            <li>- Maîtrise des techniques SEO (Search Engine Optimization) pour une meilleure visibilité sur les moteurs de recherche.</li>
+                            
+                            <li>Objectif : créer une présence en ligne captivante et efficace en respectant les meilleures pratiques du web et en suivant les dernières tendances numériques.</li>
+                          </ul>
+                         
+                        </div>
                         <div className="col-md-4">
-                          <h3>
-                           Bac +2 ENIGMA Lille
-                          </h3>
-                          <p>
-                           En cours
-                           </p>
+                          
+                          <ul style={divStyle}>
+                            <li>
+                              <h3>
+                                Bac +2 ENIGMA Lille <span>2023-2024</span>
+                             </h3>
+                            </li>
+                            <li>En Cours</li>
+                          </ul>
                          
                         </div>
                     </div>
